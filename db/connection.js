752dd3fd94fa86a4e5f422f1eca864dd3735a33c
Tuhin Roy <troy0870@gmail.com>
2020-09-05 16:25:16 +0530

    add DBaaS

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/Granip', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Granip', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
